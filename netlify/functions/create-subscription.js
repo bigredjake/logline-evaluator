797472ee84a98ec7674f1faa9ddb0f947a55cab0
bigredjake <jacob@bigredstripe.com>
2025-11-11 21:08:01 -0800

    Update create-subscription.js update key

--- netlify/functions/create-subscription.js
+++ netlify/functions/create-subscription.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY_TEST);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event, context) => {
   // Only allow POST requests
